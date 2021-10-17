f42e7e98695aced7bf754498611b0959aede3db1
shosatojp <me@shosato.jp>
2021-10-18 04:09:16 +0900

    クラス化

--- branch.lua
+++ branch.lua
@@ -1,6 +1,7 @@
 local args = {...}
 
-MOVEMENTS = {}
+-- 移動
+-- MOVEMENTS = {}
 FORWARD = 0
 BACK = 1
 UP = 2
@@ -8,24 +9,68 @@ DOWN = 3
 TURN_RIGHT = 4
 TURN_LEFT = 5
 
+-- 方角
+NORTH = 0 -- +x
+EAST = 1 -- +y
+SOUTH = 2 -- -x
+WEST = 3 -- -y
+
+DIRECTION_STRING = {
+    [NORTH] = "NORTH",
+    [EAST] = "EAST",
+    [SOUTH] = "SOUTH",
+    [WEST] = "WEST",
+}
+FORWARD_OFFSETS = {
+    [NORTH] = vector.new(1, 0, 0),
+    [EAST] = vector.new(0, 0, 1),
+    [SOUTH] = vector.new(-1, 0, 0),
+    [WEST] = vector.new(0, 0, -1),
+}
+REVERSE_MOVEMENT = {
+    [FORWARD] = BACK,
+    [BACK] = FORWARD,
+    [UP] = DOWN,
+    [DOWN] = UP,
+    [TURN_RIGHT] = TURN_LEFT,
+    [TURN_LEFT] = TURN_RIGHT,
+}
+
+-- 起動時の方角を北とする
+-- DIRECTION = NORTH
+
+-- 移動開始時を原点とする相対座標
+-- POS = vector.new(0, 0, 0)
+
+Location = {}
+
+function Location.new(position, direction)
+    local obj = { position = position, direction = direction, movements = {} }
+    return setmetatable(obj, {__index = Location})
+end
+
+function Location.print(self)
+    print("pos: (", self.position, ") , facing: ", DIRECTION_STRING[self.direction])
+end
+
 -- forcely move
-function move(m)
-    print("move "..tostring(m))
-    dig(m)
-    if move_nolog(m) then
-        table.insert(MOVEMENTS, m)
+function Location.move(self, m)
+    self:dig(m)
+    if self:move_nolog(m) then
+        table.insert(self.movements, m)
     else
         print("failed to move "..tostring(m))
+        exit()
     end
 end
 
-function move_ntimes(m, ntimes)
-    for i = 1, ntimes do
-        move(m)
-    end
-end
+-- function move_ntimes(m, ntimes)
+--     for i = 1, ntimes do
+--         move(m)
+--     end
+-- end
 
-function dig(m)
+function Location.dig(self, m)
     if m == FORWARD then
         while turtle.detect() do
             turtle.dig()
@@ -44,82 +89,95 @@ function dig(m)
     end
 end
 
-function move_nolog(m)
+function Location.move_nolog(self, m)
     local result
     if m == FORWARD then
         result = turtle.forward()
+        if result then
+            self.position = self.position + FORWARD_OFFSETS[self.direction]
+        end
     elseif m == BACK then
         result = turtle.back()
+        if result then
+            self.position = self.position + FORWARD_OFFSETS[self.direction] * -1
+        end
     elseif m == UP then
         result = turtle.up()
+        if result then
+            self.position = self.position + vector.new(0, 1, 0)
+        end
     elseif m == DOWN then
         result = turtle.down()
+        if result then
+            self.position = self.position + vector.new(0, -1, 0)
+        end
     elseif m == TURN_RIGHT then
         result = turtle.turnRight()
+        if result then
+            self.direction = (self.direction + 1) % 4
+        end
     elseif m == TURN_LEFT then
         result = turtle.turnLeft()
+        if result then
+            self.direction = (self.direction - 1) % 4
+        end
     end
+    self:print()
     return result
 end
 
-REVERSE_MOVEMENT = {
-    [FORWARD] = BACK,
-    [BACK] = FORWARD,
-    [UP] = DOWN,
-    [DOWN] = UP,
-    [TURN_RIGHT] = TURN_LEFT,
-    [TURN_LEFT] = TURN_RIGHT,
-}
-
-function move_reverse_nolog(m)
-    move_nolog(REVERSE_MOVEMENT[m])
+function Location.move_reverse_nolog(self, m)
+    self:move_nolog(REVERSE_MOVEMENT[m])
 end
 
-function return_()
+function Location.rollback(self)
     while 1 do
-        rev = table.remove(MOVEMENTS, 0)
+        rev = table.remove(self.movements, 0)
         if rev == nil then
             break
         else
-            move_reverse_nolog(rev)
+            self:move_reverse_nolog(rev)
         end
 
     end
 end
 
-function down_to_ground()
-    while 1 do
-        if not(turtle.detect()) then
-            turtle.down()
-        else
-            break
-        end
-    end
-end
+-- function down_to_ground()
+--     while 1 do
+--         if not(turtle.detect()) then
+--             turtle.down()
+--         else
+--             break
+--         end
+--     end
+-- end
 
-function line(n)
-    move(UP)
+-- 3マス分の高さで直線に掘る
+function line(n, loc)
+    loc:move(UP)
+    loc:dig(UP)
     for i = 1, n do 
-        dig(FORWARD)
-        move(FORWARD)
-        dig(DOWN)
+        loc:dig(FORWARD)
+        loc:move(FORWARD)
+        loc:dig(UP)
+        loc:dig(DOWN)
     end
-    move(DOWN)
+    loc:move(DOWN)
 end
 
-function plane(m, n)
+function plane(m, n, loc)
     for i = 1, n do
-        line(m)
+        line(m, loc)
         if i ~= n then
             -- 180度回転
             if i % 2 == 1 then
-                move(TURN_RIGHT)
-                move(FORWARD)
-                move(TURN_RIGHT)
+                loc.move(TURN_RIGHT)
+                loc.move(FORWARD)
+                loc.move(TURN_RIGHT)
             else
-                move(TURN_LEFT)
-                move(FORWARD)
-                move(TURN_LEFT)
+                loc.move(TURN_LEFT)
+                loc.move(FORWARD)
+                loc.move(TURN_LEFT)
             end
         else
             -- 終了
@@ -128,8 +186,20 @@ function plane(m, n)
     end
 end
 
--- line(args[1])
--- return_()
-plane(5, 5)
+
+local command = args[1]
+if command == "plane" then
+    loc = Location.new(vector.new(0, 0, 0), NORTH)
+    plane(args[2], args[3], loc)
+    loc:rollback()
+elseif command == "line" then
+    loc = Location.new(vector.new(0, 0, 0), NORTH)
+    line(args[2], loc)
+    loc:rollback()
+end
+
 
 -- dl https://mc.shosato.jp/branch.lua branch
+
+-- 直方体がすべて空いているものとして帰還するプログラム
+-- pos, direction, 関連操作をクラスにまとめる
